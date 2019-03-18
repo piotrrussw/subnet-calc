f8a5fa5603072bc00f20abd3f2f859093abe9a92
piotrrussw <piotr.wadycki@gmail.com>
2019-03-18 21:37:08 +0100

    add ip & mask converters, define rest methods

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ import socket
 
 
 def is_valid_ip(ip_addr):
+    """
+
+    :param ip_addr:
+    :return:
+    """
     octet_ip = ip_addr.split(".")
     int_octet_ip = [int(i) for i in octet_ip]
     if (len(int_octet_ip) == 4) and \
@@ -19,7 +24,39 @@ def is_valid_ip(ip_addr):
         exit(0)
 
 
+def is_valid_mask(mask_addr):
+    """
+
+    :param mask_addr:
+    :return:
+    """
+    int_mask = int(mask_addr)
+    if 1 <= int_mask <= 30:
+        return True
+    else:
+        return False
+
+
+def cidr_to_netmask(cidr):
+    """
+
+    :param cidr:
+    :return:
+    """
+    cidr = int(cidr)
+    mask_addr = (0xffffffff >> (32 - cidr)) << (32 - cidr)
+    return (str((0xff000000 & mask_addr) >> 24) + '.' +
+            str((0x00ff0000 & mask_addr) >> 16) + '.' +
+            str((0x0000ff00 & mask_addr) >> 8) + '.' +
+            str((0x000000ff & mask_addr)))
+
+
 def is_private_ip(ip_addr):
+    """
+
+    :param ip_addr:
+    :return:
+    """
     pv_lo = re.compile(r"^127\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
     pv_24 = re.compile(r"^10\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
     pv_20 = re.compile(r"^192\.168\.\d{1,3}.\d{1,3}$")
@@ -29,11 +66,24 @@ def is_private_ip(ip_addr):
 
 
 def ip_to_binary(ip_addr):
+    """
+
+    :param ip_addr:
+    :return:
+    """
     return ".".join(map(str, ["{0:08b}".format(int(x)) for x in ip_addr.split(".")]))
 
 
-def get_address(ip_addr):
-    return ipaddress.ip_network(ip_addr)
+def get_network_address(ip_addr, subnet_mask):
+    """
+
+    :param ip_addr:
+    :param subnet_mask:
+    :return:
+    """
+    return ".".join(map(str, [i & m  # Apply the mask
+                              for i, m in zip(map(int, ip_addr.split(".")),
+                                              map(int, subnet_mask.split(".")))]))
 
 
 def get_network_class(ip_addr):
@@ -41,6 +91,18 @@ def get_network_class(ip_addr):
 
 
 def get_broadcast_address(ip_addr):
+    return
+
+
+def get_primary_host(ip_addr):
+    return
+
+
+def get_last_host(ip_addr):
+    return
+
+
+def get_maximum_hosts(ip_addr):
     return
 
 
@@ -56,8 +118,10 @@ else:
 
 is_private = 'private' if is_private_ip(ip) else 'public'
 
-if is_valid_ip(ip):
+if is_valid_ip(ip) and is_valid_mask(mask):
     print("\nThe entered ip address is: " + ip)
     print("Ip address in binary is: " + ip_to_binary(ip))
     print("The entered ip address is: " + is_private)
-    print("The entered subnet mask is: " + mask)
+    print("The entered mask cidr is: " + mask)
+    print("The entered mask is: " + cidr_to_netmask(mask))
+    print("The entered network address is: " + get_network_address(ip, mask))
