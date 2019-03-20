a54b7b437666b223eeb49a9b2c256e0e782c8b56
piotrrussw <piotr.wadycki@gmail.com>
2019-03-20 21:39:22 +0100

    add ping & hosts methods

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import os
 import re
 import struct
 import sys
@@ -123,21 +124,26 @@ def get_broadcast_address(ip_addr, subnet_mask):
     :param subnet_mask:
     :return:
     """
-    return ".".join(map(str, [i | m  # Apply the mask
+    return ".".join(map(str, [(i | ~m) & 0xff  # Apply the mask
                               for i, m in zip(map(int, ip_addr.split(".")),
                                               map(int, subnet_mask.split(".")))]))
 
 
-def get_primary_host(ip_addr):
-    return
+def get_min_host(net_address):
+    octet_net_address = net_address.split(".")
+    octet_net_address[3] = str(int(octet_net_address[3]) + 1)
+    return ".".join(octet_net_address)
 
 
-def get_last_host(ip_addr):
-    return
+def get_max_host(broadcast_addr):
+    octet_broadcast_address = broadcast_addr.split(".")
+    octet_broadcast_address[3] = str(int(octet_broadcast_address[3]) - 1)
+    return ".".join(octet_broadcast_address)
 
 
-def get_maximum_hosts(ip_addr):
-    return
+def get_maximum_hosts(sub_mask):
+    no_zeros = to_binary(sub_mask).count("0")
+    return str(32 - no_zeros)
 
 
 arg = sys.argv
@@ -149,19 +155,66 @@ if len(arg) > 1:
     mask = cidr_to_netmask(mask)
 else:
     print('No ip address was passed, getting automatically...')
+    ip = socket.gethostbyname(socket.getfqdn())
     mask = socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM),
                                         35099, struct.pack('256s', "lo"))[20:24])
-    ip = socket.gethostbyname(socket.getfqdn())
-
-is_private = 'private' if is_private_ip(ip) else 'public'
 
 if is_valid_ip(ip) and is_valid_mask(mask):
+    binary_ip = to_binary(ip)
+    is_private = 'private' if is_private_ip(ip) else 'public'
+    cidr_mask = netmask_to_cidr(mask)
+    binary_mask = to_binary(mask)
+    network_address = get_network_address(ip, mask)
+    binary_network_address = to_binary(network_address)
+    network_class = get_network_class(ip)
+    broadcast_address = get_broadcast_address(ip, mask)
+    binary_broadcast_address = to_binary(broadcast_address)
+    maximum_hosts = get_maximum_hosts(mask)
+    min_host = get_min_host(network_address)
+    max_host = get_max_host(broadcast_address)
+
+    text_file = open("data.txt", "w")
+
     print("\nThe entered ip address is: " + ip)
-    print("Ip address in binary is: " + to_binary(ip))
+    text_file.write("The entered ip address is: " + ip + "\n")
+    print("Ip address in binary is: " + binary_ip)
+    text_file.write("Binary IP: " + binary_ip + "\n")
     print("The entered ip address is: " + is_private)
-    print("The entered mask cidr is: " + netmask_to_cidr(mask))
+    text_file.write("The entered ip address is: " + is_private + "\n")
+    print("The entered mask cidr is: " + cidr_mask)
+    text_file.write("The entered mask cidr is: " + cidr_mask + "\n")
     print("The entered mask is: " + mask)
-    print("The entered mask binary is: " + to_binary(mask))
-    print("The entered network address is: " + get_network_address(ip, mask))
-    print("The entered network class is: " + get_network_class(ip))
-    print("The entered broadcast address is: " + get_broadcast_address(ip, mask))
+    text_file.write("The entered mask is: " + mask + "\n")
+    print("The entered mask binary is: " + binary_mask)
+    text_file.write("The entered mask binary is: " + binary_mask + "\n")
+    print("The entered network address is: " + network_address)
+    text_file.write("The entered network address is: " + network_address + "\n")
+    print("The entered network address binary is: " + binary_network_address)
+    text_file.write("The entered network address binary is: " + binary_network_address + "\n")
+    print("The entered network class is: " + network_class)
+    text_file.write("The entered network class is: " + network_class + "\n")
+    print("The entered broadcast address is: " + broadcast_address)
+    text_file.write("The entered broadcast address is: " + broadcast_address + "\n")
+    print("The entered broadcast address binary is: " + binary_broadcast_address)
+    text_file.write("The entered broadcast address binary is: " + binary_broadcast_address + "\n")
+    print("The maximum number of hosts is: " + maximum_hosts)
+    text_file.write("The maximum number of hosts is: " + maximum_hosts + "\n")
+    print("The min host is: " + min_host)
+    text_file.write("The min host is: " + min_host + "\n")
+    print("The min host is: " + max_host)
+    text_file.write("The min host is: " + max_host + "\n")
+    print("\n PING?")
+    pressed = raw_input("Press Y or N\n")
+
+    text_file.close()
+
+    if pressed == 'Y':
+        response = os.system("ping -c 1 " + ip)
+        if response == 0:
+            print(ip + ", is up!")
+        else:
+            print(ip + ", is down!")
+    else:
+        print("Thank you")
+
+    exit(0)
